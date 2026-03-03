edabd54b32fe7b7a36f8d70581af08023fc51cb8
Douglas Myrie <info@hopepicenter.org>
2026-03-03 14:34:46 -0500

    Compile src/functions and use generated VERSION for /api/version

--- src/functions/version/index.ts
+++ src/functions/version/index.ts
@@ -1,6 +1,6 @@
 import { VERSION } from "./version";
 
-export async function version(context: any, req: any): Promise<void> {
+export default async function (context: any, _req: any): Promise<void> {
   context.res = {
     status: 200,
     headers: { "content-type": "application/json; charset=utf-8" },
